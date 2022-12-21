d8a323f8ba09301b30297b8631008e4122b572a3
JohannesOostenbrink <oostenhan@gmail.com>
2022-12-21 17:33:41 +0700

    Added some comments

--- async.js
+++ async.js
@@ -1,7 +1,12 @@
-const myPromise = newPromise((resolve, reject) =>{
+const myPromise = newPromise((resolve, reject) =>{ //This promise will eventually have 
+                                                   // a value. Either a resolved one or a rejected one.
+                                                // We use the resolved value if for example our API call is successful 
+                                                // Promise will pending until resolve or reject is called                                                                       
     if(true){
-           setTimeout(() =>{
-        resolve('I have succeeded');
+           setTimeout(() =>{                   // setTimeout is a function that takes the callback and a time as its second argument
+                                               
+        resolve('I have succeeded' // this can be any value
+        );
     }, 1000);
     }else {
         reject('I have failed');
@@ -12,10 +17,11 @@ const myPromise = newPromise((resolve, reject) =>{
 })
 
 myPromise
-.then(value => console.log(value))
+.then(value // this will be the value passed in resolve / or just 'value' Im not sure 
+ => console.log(value))
 .catch(rejectValue => console.log(rejectValue)); 
 
-// Promise is what is essentially says. It is a promise that the object will return a vale. 
-//It will either be resolved or rejected 
-// The resolved value we use when our API for ex is successfull 
-//The promise will be in a state that is pending when
+// With promise we can seperate successful from unsuccessful API calls 
+//promise is used because we never know if server delivered material successfully
+// When you use fetch, it returns a promise value while it makes a API request to the URL you requested
+// If error then fetch will reject promise 
